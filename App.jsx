3a231b09d93914939eae152fb3deee3e96412d3a
GiltMonster <lucassantoslukas2@gmail.com>
2023-11-12 13:29:57 -0300

    refact: adicionei as rotas iguais as do Mignon

--- App.jsx
+++ App.jsx
@@ -4,47 +4,23 @@ import Home from './app/pages/Home';
 import {useEffect} from 'react';
 import { initDatabase } from "./app/services/initDB";
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CriarExercicio from './app/pages/CriarExercicio';
 
 export default function App() {
-  const [userName, setUserName] = useState('Barbara');
-  const [height, setHeight] = useState(1.10);
-  const [weight, setWeight] = useState(43.00);
-  const [imcCategory, setImcCategory] = useState("Peso Normal");
-  const [idealWeight, setIdealWeight] = useState("41,63 Kg - 56,25 Kg");
 
-  const calculatesImc = (weight, height) => {
-    const imcValue = weight / Math.pow(height,2)
+  const Stack = createNativeStackNavigator();
 
-    if (imcValue < 18.5) {
-      setImcCategory("Abaixo do Peso");
-    } else if (imcValue < 25) {
-      setImcCategory("Peso Normal");
-    } else if (imcValue < 30) {
-      setImcCategory("Acima do Peso");
-    } else if (imcValue >= 30){
-      setImcCategory("Obesidade");
-    } else {
-      message = 'Os valores informados são inválidos!'
-    }
-  }
-
-  const onPress = () => {
-
-  }
 
   return (
-    <View style={styles.container}>
-      <Home 
-        onPress={onPress}
-        userName={userName} 
-        trainingName={'Costas e Bíceps'} 
-        members={'Superior'}
-        height={height}
-        weight={weight}
-        imcCategory={imcCategory}
-        idealWeight={idealWeight}
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="CriarExercicio" component={CriarExercicio}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
