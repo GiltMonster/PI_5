7d6146b4ff3f2ce4938fc49eaa781ff934c66721
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2023-11-20 14:35:14 -0300

    refactor: meta atualizada para quando não tiver dados cadastrados

--- app/components/Goal.jsx
+++ app/components/Goal.jsx
@@ -1,61 +1,73 @@
-import { View, StyleSheet, Dimensions } from 'react-native';
-import Svg, { Rect, Line, Text } from 'react-native-svg';
+import { View, Text , StyleSheet, Dimensions } from 'react-native';
+import Svg, { Rect, Line, Text as SvgText} from 'react-native-svg';
 import replace from '../util/replace';
+import getFont from '../util/fonts';
 
 export default function Goal({ currentWeight, targetWeight }) {
-    const heightContainer = currentWeight > targetWeight ? currentWeight+50 : targetWeight+50;
+  const heightContainer = currentWeight > targetWeight ? currentWeight+50 : targetWeight+50;
+  const noGoal = currentWeight <= 0 || targetWeight <= 0 ? true : false;
 
   return (
-    <View style={styles.goalContainer}>
-      <Svg width={300} height={heightContainer}>
-        <Rect
-          x={(300 - 33) / 2}
-          y={heightContainer - currentWeight}
-          width={33}
-          height={currentWeight}
-          fill={'#8CBECF'}
-        />
-        <Rect
-          x={(300 - 33) / 2}
-          y={heightContainer - targetWeight}
-          width={33}
-          height={targetWeight}
-          fill={'#28A3CC'}
-        />
-        <Line
-          x1={(333) / 2}
-          y1={heightContainer - currentWeight}
-          x2={0}
-          y2={heightContainer - currentWeight}
-          stroke="#E3ECF1"
-          strokeWidth="1"
-        />
-        <Line
-          x1={300}
-          y1={heightContainer - targetWeight}
-          x2={133}
-          y2={heightContainer - targetWeight}
-          stroke="#E3ECF1"
-          strokeWidth="1"
-        />
-        <Text
-          x={0} 
-          y={heightContainer - currentWeight - 5} 
-          fill="#E3ECF1"
-          fontSize="13"
-        >
-          Peso Atual: {replace(currentWeight)} Kg
-        </Text>
-        <Text
-          x={225} 
-          y={heightContainer - targetWeight - 5} 
-          fill="#00E0FF"
-          fontSize="13"
-        >
-          Meta: {replace(targetWeight)} Kg
-        </Text>
-      </Svg>
-      <View style={styles.line}></View>
+    <View>
+      {noGoal && (
+        <View style={styles.noGoal}>
+          <Text style={[styles.noGoalText, {fontFamily: getFont('sfProTextSemibold')} ]}>SEM DADOS REGISTRADOS</Text>
+        </View>
+      )}
+
+      {!noGoal && (
+        <View style={styles.goalContainer}>
+          <Svg width={300} height={heightContainer}>
+            <Rect
+              x={(300 - 33) / 2}
+              y={heightContainer - currentWeight}
+              width={33}
+              height={currentWeight}
+              fill={'#8CBECF'}
+            />
+            <Rect
+              x={(300 - 33) / 2}
+              y={heightContainer - targetWeight}
+              width={33}
+              height={targetWeight}
+              fill={'#28A3CC'}
+            />
+            <Line
+              x1={(333) / 2}
+              y1={heightContainer - currentWeight}
+              x2={0}
+              y2={heightContainer - currentWeight}
+              stroke="#E3ECF1"
+              strokeWidth="1"
+            />
+            <Line
+              x1={300}
+              y1={heightContainer - targetWeight}
+              x2={133}
+              y2={heightContainer - targetWeight}
+              stroke="#E3ECF1"
+              strokeWidth="1"
+            />
+            <SvgText
+              x={0} 
+              y={heightContainer - currentWeight - 5} 
+              fill="#E3ECF1"
+              fontSize="13"
+            >
+              Peso Atual: {replace(currentWeight)} Kg
+            </SvgText>
+            <SvgText
+              x={225} 
+              y={heightContainer - targetWeight - 5}
+              fill="#00E0FF"
+              fontSize="13"
+            >
+              Meta: {replace(targetWeight)} Kg
+            </SvgText>
+          </Svg>
+          <View style={styles.line}></View>
+        </View>
+      )}
     </View>
   );
 }
@@ -74,4 +86,17 @@ const styles = StyleSheet.create({
     width: '90%',
     marginBottom: 10
   },
+  noGoal: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    backgroundColor: '#232325',
+    borderRadius: 10,
+    paddingVertical: 10,
+    height: 150
+  },
+  noGoalText: {
+    color: 'gray',
+    fontSize: 17,
+  }
 });
