e1fc872b64c8e68e72b21362d1ee5dd4d67817ec
Lucas S. Campos <71074779+GiltMonster@users.noreply.github.com>
2023-12-11 00:34:26 -0300

    Feat: gera a lista dos treinos diretamente da api

--- app/pages/TelaTreino.jsx
+++ app/pages/TelaTreino.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, ScrollView, Alert, FlatList } from 'react-native';
 import CardTreino from '../components/CardTreino';
 import HeaderScreensNavigations from '../components/HeaderScreensNavigations';
+import { getTreinos } from '../services/TreinoDB';
 
-const TelaTreino = ({ route, navigation }) => {
-  const treinoSalvo = route.params?.treinoSalvo || null;
+const TelaTreino = ({ navigation }) => {
+  const [treino, setTreino] = useState();
+
+  useEffect(() => {
+    getTreinos().then((treinos) => {
+      if (!treinos) {
+        Alert.alert('Atenção', 'Não foi possível carregar os treinos.', ['ok']);
+      } else {
+        console.log('Treinos carregados com sucesso.');
+        setTreino(treinos);
+      }
+    });
+  }, []);
+
+  const goToPage = () => {
+    navigation.navigate('criarTreino');
+  }
+
   return (
     <View style={styles.container}>
-            <HeaderScreensNavigations
+      <HeaderScreensNavigations
         title="Criar Treino"
-        onAddPress={() => navigation.navigate('criarTreino')}
+        onAddPress={goToPage}
       />
-      <ScrollView>
-        {treinoSalvo && (
-          <CardTreino
-            titulo={treinoSalvo.titulo}
-            categoria={treinoSalvo.categoria}
-            diaSemana={treinoSalvo.diaDaSemana}
-            isChecked={false}
-            onCheckboxToggle={() => { }}
-            treinoSalvo={treinoSalvo} 
-            navigation={navigation}
-
+        {treino && (
+          <FlatList
+            data={treino}
+            renderItem={({ item }) => <CardTreino
+              onCheckboxToggle={() => { }}
+              treino={item}
+              navigation={navigation}
+            />}
+            keyExtractor={item => item.idTreino}
           />
         )}
-      </ScrollView>
     </View>
   );
 };
