472bf090969214c014468a49bd43ef98ad416900
Lucas S. Campos <71074779+GiltMonster@users.noreply.github.com>
2023-12-12 00:49:44 -0300

    Revert "feat: cadastro do exercicio feito"

--- app/util/consts.js
+++ app/util/consts.js
@@ -1,4 +1,4 @@
-export const IP = '192.168.10.7';
+export const IP = '192.168.10.19';
 export const API_URL_GET_USUARIO = `http://${IP}:3000/usuario/usuario`;
 export const API_URL_CADASTRAR_USUARIO = `http://${IP}:3000/usuario/cadastrarUsuario`;
 
@@ -7,11 +7,5 @@ export const API_URL_GET_TREINO = `http://${IP}:3000/treino/getTreino`;
 export const API_URL_CADASTRAR_TREINO = `http://${IP}:3000/treino/cadastrarTreino`;
 
 //EXERCICIO
-export const API_URL_GET_EXERCICIO = `http://${IP}:3000/exercicio/getExercicio`;
-export const API_URL_GET_EXERCICIO_POR_TREINO = `http://${IP}:3000/exercicio/getExerciciosPeloTreino`;
 export const API_URL_CADASTRAR_EXERCICIO = `http://${IP}:3000/exercicio/cadastrarExercicio`;
-
-// DESCRICAO
-export const API_URL_GET_DESCRICAO = `http://${IP}:3000/descricaoExercicio/getDescricaoExercicio`;
-export const API_URL_CADASTRA_DESCRICAO = `http://${IP}:3000/descricaoExercicio/cadastrarDescricaoExercicio`;
 
